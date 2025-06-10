9c2303e892d5471f6f953a76af015b04a9d3d08e
Abhijnya002 <53806883+Abhijnya002@users.noreply.github.com>
2025-06-10 18:32:28 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/AI-powered-data-extracter/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
